9ef4e6fe0ee4c14139badc6eb89655c09c227010
Oksanen Pihla M <okpiok@da2-ex-dk107-05.helsinki.fi>
2015-09-09 13:51:37 +0300

    Näkymiä suunniteltu

--- app/controllers/hello_world_controller.php
+++ app/controllers/hello_world_controller.php
@@ -1,14 +1,37 @@
 <?php
 
-  class HelloWorldController extends BaseController{
+class HelloWorldController extends BaseController {
 
-    public static function index(){
-      // make-metodi renderöi app/views-kansiossa sijaitsevia tiedostoja
-   	  View::make('home.html');
+    public static function index() {
+        // make-metodi renderöi app/views-kansiossa sijaitsevia tiedostoja
+        echo 'Tämä on etusivu!';
     }
 
-    public static function sandbox(){
-      // Testaa koodiasi täällä
-      echo 'Hello World!';
+    public static function sandbox() {
+        // Testaa koodiasi täällä
+        View::make('helloworld.html');
     }
-  }
+    
+        public static function rekisteroidy() {
+        View::make('suunnitelmat/rekisteroidy.html');
+    }
+
+    public static function login() {
+        View::make('suunnitelmat/login.html');
+    }
+
+    public static function tehtavat_list() {
+        View::make('suunnitelmat/tehtavat_list.html');
+    }
+
+    public static function etusivu() {
+        View::make('suunnitelmat/etusivu.html');
+    }
+
+    public static function muokkaus() {
+        View::make('suunnitelmat/muokkaus.html');
+    }
+    public static function nayta_tehtava() {
+        View::make('suunnitelmat/nayta_tehtava.html');
+    }
+}
--- config/routes.php
+++ config/routes.php
@@ -1,9 +1,28 @@
 <?php
 
-  $routes->get('/', function() {
-    HelloWorldController::index();
-  });
+$routes->get('/', function() {
+    HelloWorldController::etusivu();
+});
 
-  $routes->get('/hiekkalaatikko', function() {
+$routes->get('/hiekkalaatikko', function() {
     HelloWorldController::sandbox();
-  });
+});
+
+$routes->get('/tehtavat_list', function() {
+    HelloWorldController::tehtavat_list();
+});
+
+$routes->get('/login', function() {
+    HelloWorldController::login();
+});
+
+$routes->get('/muokkaus', function() {
+    HelloWorldController::muokkaus();
+});
+
+$routes->get('/nayta_tehtava', function() {
+    HelloWorldController::nayta_tehtava();
+});
+$routes->get('/rekisteroidy', function() {
+    HelloWorldController::rekisteroidy();
+});
