2aadd6a6fcad905f5150a144c76826a8cd0726e7
Oksanen Pihla M <okpiok@da2-ex-dk107-06.helsinki.fi>
2015-09-10 12:52:56 +0300

    Tietokantataulujen pystytys-ja poistolauseiden sekä testidatan lisäys

--- sql/add_test_data.sql
+++ sql/add_test_data.sql
@@ -1,1 +1,32 @@
 -- Lisää INSERT INTO lauseet tähän tiedostoon
+
+--KÄYTTÄJÄ
+
+INSERT INTO Kayttaja (kayttajatunnus, salasana, etunimi, sukunimi, sahkoposti)
+VALUES('Testi', 'abc123', 'Testaaja', 'Testaus', 'abc@sähköposti.fi');
+
+INSERT INTO Kayttaja (kayttajatunnus, salasana, etunimi, sukunimi, sahkoposti) 
+VALUES ('Testi2', 'asd123','Testaaja2', 'Testaus2', 'abc2@sähköposti.fi');
+
+--TÄRKEYSASTE
+
+INSERT INTO Tarkeysaste(nimi) VALUES('Erittäin tärkeä');
+
+-- TEHTÄVÄ
+
+INSERT INTO Tehtava(otsikko, kuvaus, tarkeysaste_id,ajankohta, kayttaja_id)
+VALUES('Tsoha', 'Palauta harjoitustyo', 1, '18-10-2015 23:59', 1);
+
+--LUOKKA
+INSERT INTO Luokka(nimi, kuvaus,kayttaja_id) VALUES('Opiskelu', 'Opinnot Helsingin yliopistossa', 1);
+
+INSERT INTO Luokka(nimi,kayttaja_id) VALUES('Työ', 1);
+
+INSERT INTO Luokka(nimi, kayttaja_id) VALUES('Vapaa-aika',2);
+
+--TEHTÄVÄLUOKKA
+
+INSERT INTO Tehtavaluokka(tehtava_id, luokka_id) VALUES(1,1);
+
+INSERT INTO Tehtavaluokka(tehtava_id, luokka_id) VALUES(1,2);
+
--- sql/create_tables.sql
+++ sql/create_tables.sql
@@ -1,1 +1,43 @@
--- Lisää CREATE TABLE lauseet tähän tiedostoon
+
+CREATE TABLE Kayttaja(
+id SERIAL PRIMARY KEY NOT NULL,
+kayttajatunnus VARCHAR(50) NOT NULL,
+salasana VARCHAR(50) NOT NULL,
+etunimi VARCHAR(50) NOT NULL,
+sukunimi VARCHAR(50) NOT NULL,
+sahkoposti VARCHAR(100) NOT NULL
+);
+
+CREATE TABLE Tarkeysaste(
+id SERIAL PRIMARY KEY NOT NULL,
+nimi VARCHAR(100) NOT NULL,
+kuvaus VARCHAR(1000)
+);
+
+CREATE TABLE Luokka(
+id SERIAL PRIMARY KEY NOT NULL,
+nimi VARCHAR(100) NOT NULL,
+kuvaus VARCHAR(1000),
+kayttaja_id INTEGER NOT NULL,
+CONSTRAINT fk_kayttaja_id FOREIGN KEY(kayttaja_id) REFERENCES Kayttaja(id) 
+);
+
+CREATE TABLE Tehtava(
+id SERIAL PRIMARY KEY NOT NULL,
+otsikko VARCHAR(200) NOT NULL,
+kuvaus VARCHAR(2000),
+suoritettu BOOLEAN DEFAULT FALSE NOT NULL,
+ajankohta TIMESTAMP,
+tarkeysaste_id INTEGER, 
+kayttaja_id INTEGER NOT NULL,
+CONSTRAINT fk_kayttaja_id FOREIGN KEY(kayttaja_id) REFERENCES Kayttaja(id),
+CONSTRAINT fk_tarkeysaste_id FOREIGN KEY(tarkeysaste_id) REFERENCES Tarkeysaste(id)
+); 
+
+CREATE TABLE Tehtavaluokka(
+tehtava_id INTEGER NOT NULL,
+luokka_id INTEGER NOT NULL,
+CONSTRAINT fk_tehtava_id FOREIGN KEY(tehtava_id) REFERENCES Tehtava(id),
+CONSTRAINT fk_luokka_id FOREIGN KEY(luokka_id) REFERENCES Luokka(id),
+CONSTRAINT pk_tehtavaluokka PRIMARY KEY(tehtava_id, luokka_id)
+);
